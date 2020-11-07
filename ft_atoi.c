2ec2d63b055bb5a25bc9916ce7157e1a038a24ce
Nestor Cofre <ncofre98@gmail.com>
2020-11-07 08:32:57 -0600

    ft_atoi.c Added

---
+++ ft_atoi.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncofre <ncofre@student.42.us.org>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/07 00:01:51 by ncofre            #+#    #+#             */
+/*   Updated: 2020/11/07 08:32:33 by ncofre           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(const char *nptr)
+{
+	int i;
+	int sign;
+	int number;
+
+	i = 0;
+	sign = 1;
+	number = 0;
+	while (nptr[i] == ' ' || (nptr[i] >= '\t' && nptr[i] <= '\r'))
+		i++;
+	if (nptr[i] == '+')
+		i++;
+	else if (nptr[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		number = number * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return (number * sign);
+}
