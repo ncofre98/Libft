21323caeff04fd73ee8413dd9b300370179d9a4a
Nestor Cofre <ncofre98@gmail.com>
2020-11-22 00:05:07 -0600

    ft_strnstr.c added and fixed the looping while searching next ocurrence of character

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,12 +6,11 @@
 /*   By: ncofre <ncofre@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 17:20:33 by ncofre            #+#    #+#             */
-/*   Updated: 2020/11/21 23:18:03 by ncofre           ###   ########.fr       */
+/*   Updated: 2020/11/22 00:04:09 by ncofre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 /*
 	This function locates the first ocurrence of the NULL-terminated string s2
@@ -44,16 +43,7 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 		if (ft_strncmp(f_letter, str, ft_strlen(str)) == 0)
 			return (f_letter);
 		else
-			f_letter = ft_strchr(f_letter, str[0]);
+			f_letter = ft_strchr(f_letter + 1, str[0]);
 	}
 	return (NULL);
-}
-
-int	main(void)
-{
-	char s1[] = "El gato blanco Solovino :3";
-	char s2[] = "Solovino";
-
-	printf("%s\n", ft_strnstr(s1, s2, sizeof(s2) - 1));
-	return (0);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ncofre <ncofre@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/07 09:49:00 by ncofre            #+#    #+#             */
-/*   Updated: 2020/11/21 22:36:25 by ncofre           ###   ########.fr       */
+/*   Updated: 2020/11/22 00:04:21 by ncofre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char    *ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
-/* char	*ft_strnstr(const char *s1, const char *s2, size_t n); */
+char	*ft_strnstr(const char *s1, const char *s2, size_t n);
 int     ft_strncmp(const char *s1, const char *s2, size_t n);
 int	ft_atoi(const char *nptr);
 int	ft_isalpha(int c);
