c10f21967e387b7312e71ad4f81a65a706120686
Nestor Cofre <ncofre98@gmail.com>
2020-11-24 23:12:32 -0600

    ft_substr.c done

---
+++ ft_substr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncofre <ncofre@student.42.us.org>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/24 22:19:45 by ncofre            #+#    #+#             */
+/*   Updated: 2020/11/24 23:04:47 by ncofre           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char *ptr;
+	unsigned int end;
+	unsigned int i;
+
+	ptr = (char*)malloc(sizeof(char) * len + 1);
+	if (!ptr)
+		return (NULL);
+	else
+	{
+		i = 0;
+		end = start + len - 1;
+		while (start <= end)
+		{
+			ptr[i] = s[start];
+			i++;
+			start++;
+		}
+		ptr[i] = '\0';
+		return (ptr);
+	}
+}
