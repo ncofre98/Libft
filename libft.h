49801fc02a3e85a7a09732e6a576af9642464afb
Nestor Cofre <ncofre98@gmail.com>
2020-12-03 22:01:17 -0600

    ft_putchar_fd done

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ncofre <ncofre@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/07 09:49:00 by ncofre            #+#    #+#             */
-/*   Updated: 2020/11/30 09:01:13 by ncofre           ###   ########.fr       */
+/*   Updated: 2020/12/03 21:41:44 by ncofre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,7 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-/* void	ft_putchar_fd(char c, int fd); */
+void	ft_putchar_fd(char c, int fd);
 /* void	ft_putstr_fd(char *s, int fd); */
 /* void	ft_putendl_fd(char *s, int fd); */
 /* void	ft_putnbr_fd(int n, int fd); */
