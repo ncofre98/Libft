6374ccbf3db728742778413140d9c0cee61b853b
Nestor Cofre <ncofre98@gmail.com>
2020-12-03 22:31:17 -0600

    ft_putendl_fd.c done

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ncofre <ncofre@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/07 09:49:00 by ncofre            #+#    #+#             */
-/*   Updated: 2020/12/03 22:01:30 by ncofre           ###   ########.fr       */
+/*   Updated: 2020/12/03 22:19:03 by ncofre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,7 @@ char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
-/* void	ft_putendl_fd(char *s, int fd); */
+void	ft_putendl_fd(char *s, int fd);
 /* void	ft_putnbr_fd(int n, int fd); */
 
 /* Additional Useful Functions */
