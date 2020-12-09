e0dd913dbbc663e08a6d612a82a76aba91e5edbd
Nestor Cofre <ncofre98@gmail.com>
2020-12-09 12:15:08 -0600

    ft_lstnew.c done

---
+++ ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncofre <ncofre@student.42.us.org>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/09 11:51:12 by ncofre            #+#    #+#             */
+/*   Updated: 2020/12/09 12:12:07 by ncofre           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*ptr;
+
+	ptr = malloc(sizeof(t_list));
+	if (!ptr)
+		return (NULL);
+	ptr->content = content;
+	ptr->next = NULL;
+	return (ptr);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ncofre <ncofre@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/07 09:49:00 by ncofre            #+#    #+#             */
-/*   Updated: 2020/12/08 19:51:22 by ncofre           ###   ########.fr       */
+/*   Updated: 2020/12/09 12:01:01 by ncofre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,12 @@
 
 #include <unistd.h>
 #include <stdlib.h>
+
+typedef struct		s_list
+{
+	void			*content;
+	struct s_list 	*next;
+}					t_list;
 
 /* Libc functions*/
 
@@ -56,6 +62,17 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+
+/* Bonus */
+t_list	*ft_lstnew(void *content);
+/* void	ft_lstadd_front(t_list **lst, t_list *new); */
+/* int	ft_lstsize(t_list *lst); */
+/* t_list	*ft_lstlast(t_list *lst); */
+/* void	ft_lstadd_back(t_list **lst, t_list *new); */
+/* void	ft_lstdelone(t_list *lst, void (*del)(void *)); */
+/* void	ft_lstclear(t_list **lst, void (*del)(void *)); */
+/* void	ft_lstiter(t_list *lst, void (*f)(void *)); */
+/* t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *)); */
 
 /* Additional Useful Functions */
 int	ft_strcmp(const char *s1, const char *s2);
