8282b67687dd09152401d583e0817ae651900d9c
Nestor Cofre <ncofre98@gmail.com>
2020-12-24 00:34:50 -0600

    ft_strnstr.c tested and fixed

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: ncofre <ncofre@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 17:20:33 by ncofre            #+#    #+#             */
-/*   Updated: 2020/12/18 08:58:28 by ncofre           ###   ########.fr       */
+/*   Updated: 2020/12/24 00:34:22 by ncofre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,26 +24,18 @@
 
 char		*ft_strnstr(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
-	char	*f_letter;
-	char	str[n];
+	size_t	len;
 
-	if (!*s2)
-		return ((char*)&(s1[0]));
-	i = 0;
-	while (i < n)
+	if (!*s2 || !s2)
+		return ((char*)&*s1);
+	len = ft_strlen(s2);
+	while (*s1 && n >= len)
 	{
-		str[i] = s2[i];
-		i++;
-	}
-	str[i] = '\0';
-	f_letter = ft_strchr(s1, str[0]);
-	while (*f_letter)
-	{
-		if (ft_strncmp(f_letter, str, ft_strlen(str)) == 0)
-			return (f_letter);
-		else
-			f_letter = ft_strchr(f_letter + 1, str[0]);
+		if (*s1 == s2[0])
+			if (ft_strncmp(s1, s2, len) == 0)
+				return ((char*)&*s1);
+		s1++;
+		n--;
 	}
 	return (NULL);
 }
