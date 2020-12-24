3a19088e2972b1141ad4693b82ce74bb6a3b615f
Nestor Cofre <ncofre98@gmail.com>
2020-12-24 10:42:38 -0600

    ft_putnbr_fd.c tested and fixed

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: ncofre <ncofre@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 22:31:38 by ncofre            #+#    #+#             */
-/*   Updated: 2020/12/03 22:37:44 by ncofre           ###   ########.fr       */
+/*   Updated: 2020/12/24 10:41:27 by ncofre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,19 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n < 0)
+	long int	number;
+
+	number = n;
+	if (number < 0)
 	{
 		ft_putchar_fd('-', fd);
-		n *= -1;
+		number *= -1;
 	}
-	if (n < 10)
-		ft_putchar_fd(n + '0', fd);
+	if (number < 10)
+		ft_putchar_fd(number + '0', fd);
 	else
 	{
-		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n % 10, fd);
+		ft_putnbr_fd(number / 10, fd);
+		ft_putnbr_fd(number % 10, fd);
 	}
 }
