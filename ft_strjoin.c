e740bbb2a6743c5edad695ca570e1ff9731eb2a1
Nestor Cofre <ncofre98@gmail.com>
2020-12-27 00:52:34 -0600

    ft_strjoin.c tested and fixed

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: ncofre <ncofre@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/24 23:15:18 by ncofre            #+#    #+#             */
-/*   Updated: 2020/12/18 09:02:45 by ncofre           ###   ########.fr       */
+/*   Updated: 2020/12/27 00:51:37 by ncofre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,15 @@
 char		*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*ptr;
-	size_t	size;
+	size_t	s1_len;
+	size_t	s2_len;
 
-	size = ft_strlen(s1) + ft_strlen(s2) + 1;
-	ptr = (char*)malloc(sizeof(char) * size);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	ptr = (char*)malloc(sizeof(char) * (s1_len + s2_len + 1));
 	if (!ptr)
 		return (NULL);
-	ft_memcpy(ptr, s1, ft_strlen(s1));
-	ft_strlcat(ptr, s2, ft_strlen(s2) + 1);
+	ft_memcpy(ptr, s1, s1_len);
+	ft_strlcat(ptr, s2, s1_len + s2_len + 1);
 	return (ptr);
 }
