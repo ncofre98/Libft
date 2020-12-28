3adc8fecace69ccbc14ac49623f6664737c70467
Nestor Cofre <ncofre98@gmail.com>
2020-12-27 22:26:56 -0600

    ft_strtrim.c tested and fixed

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ncofre <ncofre@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/25 00:16:15 by ncofre            #+#    #+#             */
-/*   Updated: 2020/12/18 23:21:25 by ncofre           ###   ########.fr       */
+/*   Updated: 2020/12/27 22:25:24 by ncofre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,10 @@ static int		ft_inarray(const char c, const char *array)
 
 char			*ft_strtrim(char const *s1, char const *set)
 {
-	size_t i;
+	int i;
+	int start;
+	int end;
 	size_t size;
-	size_t start;
-	size_t end;
 
 	i = 0;
 	size = 0;
@@ -42,9 +42,11 @@ char			*ft_strtrim(char const *s1, char const *set)
 		i++;
 	start = i;
 	i = ft_strlen(s1) - 1;
-	while (s1[i] && ft_inarray(s1[i], set) == 1)
+	while (i >= 0 && ft_inarray(s1[i], set) == 1)
 		i--;
 	end = i;
+	if (end < start)
+		return (ft_substr("", 0, 1));
 	size = end - start + 1;
 	return (ft_substr(s1, start, size));
 }
