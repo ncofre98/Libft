5097135504881ba28ef7ec9aa09bdd4c6fe61e21
Nestor Cofre <ncofre98@gmail.com>
2020-12-29 01:51:04 -0600

    fixed ft_strcmp.c

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: ncofre <ncofre@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/21 21:24:16 by ncofre            #+#    #+#             */
-/*   Updated: 2020/12/09 13:25:27 by ncofre           ###   ########.fr       */
+/*   Updated: 2020/12/29 01:50:43 by ncofre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,11 @@ int	ft_strcmp(const char *s1, const char *s2)
 	size_t i;
 
 	i = 0;
-	while (s1[i] && s1[i] == s2[i])
+	while (s1[i] == s2[i])
+	{
+		if (s1[i] == '\0' && s2[i] == '\0')
+			return (0);
 		i++;
-	return (s1[i] - s2[i]);
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
